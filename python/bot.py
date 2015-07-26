0b141c9fa1a0d246d1f4fe190b324b51eb581a41
Ben Nuttall <ben@bennuttall.com>
2015-07-26 13:09:59 +0200

    Add bot

---
+++ python/bot.py
@@ -0,0 +1,22 @@
+import pyjokes
+from twython import Twython
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+def main():
+    joke = pyjokes.get_joke()
+    twitter.update_status(status=joke)
+
+if __name__ == '__main__':
+    main()
